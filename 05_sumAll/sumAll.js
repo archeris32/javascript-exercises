ae3195a6a8c9bb335f8f0d01366447d9c705b9c4
archeris <adascalului.bogdan@gmail.com>
2025-04-18 06:34:37 -0400

    first changes

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+    let sum = 0;
+    if(x<0 || y<0 || Number.isInteger(x)=== false|| Number.isInteger(y)=== false  ){
+        return 'ERROR'
+    }
+    if (x>y){
+        for (let i =y;i<=x;i++){
+
+            sum+=i;
+    }}
+    else
+    {
+        for (let i =x;i<=y;i++){
+
+        sum+=i;
+        }
+    }
+    return sum;
 
 };
 
